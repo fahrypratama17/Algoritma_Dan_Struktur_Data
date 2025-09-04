27aa5882961daeb309d45970cb5cc34850b2e22a
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-09-04 20:26:32 +0700

    feat: adding various methods

--- Singly_Lingked_List/src/Main.java
+++ Singly_Lingked_List/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-    Node head, tail;
-    int size = 0;
 
     public static void main(String[] args) {
-        Node head = new Node("A", "I");
-        Node tail = new Node("B");
+        Node list = new Node();
+
+        System.out.println("Head " + list.printSLL);
     }
 }
 
@@ -12,13 +11,63 @@ class Node {
     Object data;
     Node next;
 
+    Node head, tail;
+    int size = 0;
+
+    Node () {}
+
     Node (Object data) {
         this.data = data;
     }
 
-    Node(Object data, Node next) {
+    Node (Object data, Node next) {
         this.data = data;
         this.next = next;
+    }
+
+    void intialized () {
+        head = tail = null;
+    }
+
+    boolean isEmpty () {
+        return (size == 0);
+    }
+
+    int size() {
+        return size;
+    }
+
+    void addFirst (Object data) {
+        Node item = new Node(data);
+        if (isEmpty() ) {
+            head = item;
+            tail = item;
+        } else {
+            item.next = head;
+            head = item;
+        }
+        size++;
+    }
+
+    void addLast (Object data) {
+        Node item = new Node(data);
+        if (isEmpty()) {
+            head = tail = item;
+        } else {
+            tail.next = item;
+            tail = item;
+        }
+        size++;
+    }
+
+    public void printSLL() {
+        Node temp = head;
+        if (isEmpty()) {
+            System.out.println("Kosong nih");
+        } while (temp != null) {
+            System.out.println(temp.data = " - ");
+            temp = temp.next;
+        }
     }
 }
 
