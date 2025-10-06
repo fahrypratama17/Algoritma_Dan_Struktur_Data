68a8382757436d87538dbe5af767c1f2c982eb40
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-10-06 14:16:35 +0700

    feat: EditTeks.java

--- Code_Algoritma_Dan_Struktur_Data/Latihan_UTP/DLL/Edit_Teks/src/EditTeks.java
+++ Code_Algoritma_Dan_Struktur_Data/Latihan_UTP/DLL/Edit_Teks/src/EditTeks.java
@@ -9,36 +9,50 @@ public class EditTeks {
         System.out.println("1. Tambah Versi");
         System.out.println("2. Undo");
         System.out.println("3. Redo");
+        System.out.println("4. Tampilkan Versi Sekarang");
         System.out.println("0. Keluar");
 
         boolean flag = true;
 
         while (flag){
             System.out.print("\nPilih menu: ");
-            int choice = input.nextInt();
+            int choice = input.nextInt(); input.nextLine();
 
             switch (choice) {
                 case 1: {
+                    System.out.print("Masukkan teks: ");
+                    String teks = input.nextLine();
+
+                    Teks t = new Teks(teks);
+                    d.tambahVersi(t);
                     break;
                 }
 
                 case 2: {
+                    d.undo();
                     break;
                 }
 
                 case 3: {
+                    d.redo();
+                    break;
+                }
+
+                case 4: {
+                    d.tampiilkanVersiSekarang();
                     break;
                 }
 
                 case 0: {
                     System.out.println("Keluar dari program...");
-                    System.out.println("");
+                    System.out.println("Terima kasih dan Selamat Tinggal!!!");
                     flag = false;
                     break;
                 }
 
                 default: {
-
+                    System.out.println("Pilihan hanya dari 0-3");
+                    flag = false;
                 }
             }
         }
@@ -79,6 +93,49 @@ class DLL {
     void tambahVersi(Teks data){
         Node versiBaru = new Node(data);
 
+        if (isEmpty()){
+            head = tail = current = versiBaru;
+            size++;
+        } else {
+            tail.next = versiBaru;
+            versiBaru.prev = tail;
+            tail = current = versiBaru;
+            size++;
+        }
+    }
 
+    void undo() {
+        if (isEmpty()) {
+            System.err.println("Teks kosong!!!");
+            return;
+        }
+
+        if (current.prev == null){
+            System.err.println(current.data.teks + " sudah paling belakang!!!");
+        } else {
+            current = current.prev;
+        }
+    }
+
+    void redo() {
+        if (isEmpty()) {
+            System.err.println("Teks kosong!!!");
+            return;
+        }
+
+        if (current.next == null){
+            System.err.println(current.data.teks + " sudah paling belakang!!!");
+        } else {
+            current = current.next;
+        }
+    }
+
+    void tampiilkanVersiSekarang() {
+        if (isEmpty()) {
+            System.err.println("Teks kosong!!!");
+            return;
+        }
+
+        System.out.println("Versi sekarang: " + current.data.teks);
     }
 }
