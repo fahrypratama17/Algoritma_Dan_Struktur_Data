ed3c1e066400ec2bd07a1b58e4fb4adaaf15f007
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-10-23 16:40:50 +0700

    fix: TowerDefenseLabyrinth.java Done

--- Code_Algoritma_Dan_Struktur_Data/Tower_Defense_Labyrinth/src/TowerDefenseLabyrinth.java
+++ Code_Algoritma_Dan_Struktur_Data/Tower_Defense_Labyrinth/src/TowerDefenseLabyrinth.java
@@ -1,30 +1,61 @@
 // ---------------------------------------------
-// Tugas 1 - Tower Defense Labyrinth
+// Tugas 1 - Tower Defense Labyrinth (OPTIMIZED)
 // ---------------------------------------------
 
 import java.util.Scanner;
 
 public class TowerDefenseLabyrinth {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
 
+        Tower blueTowerData = TowerInput(input);
+        Tower redTowerData = TowerInput(input);
+
+        input.close();
+
+        int blueTowerFastest = TowerData(blueTowerData, "BLUE");
+        int redTowerFastest = TowerData(redTowerData, "RED");
+
+        System.out.println();
+
+        if (blueTowerFastest < redTowerFastest) {
+            System.out.println("BLUE TOWER TEAM IS WINNER");
+            System.out.print(blueTowerFastest + " steps vs " + redTowerFastest + " steps");
+        } else if (redTowerFastest < blueTowerFastest) {
+            System.out.println("RED TOWER TEAM IS WINNER");
+            System.out.print(redTowerFastest + " steps vs " + blueTowerFastest + " steps");
+        } else {
+            System.out.print("BOTH TEAMS ARE TIED, NO WINNER");
+        }
+    }
+
+    static Tower TowerInput(Scanner input) {
         int sizeLabirin = Integer.parseInt(input.nextLine());
+        String koorBisaDilalui = input.nextLine();
+        int jmlPeserta = Integer.parseInt(input.nextLine());
+
+        String[] dataPeserta = new String[jmlPeserta];
+        for (int i = 0; i < jmlPeserta; i++) {
+            dataPeserta[i] = input.nextLine().trim();
+        }
+        return new Tower(sizeLabirin, koorBisaDilalui, jmlPeserta, dataPeserta);
+    }
+
+    static int TowerData(Tower data, String team) {
+        int sizeLabirin = data.sizeLabirin;
         Labirin lab = new Labirin(sizeLabirin);
 
-        String bisaDilalui = input.nextLine();
-        String[] koordinatBD = bisaDilalui.split(" ");
+        String[] koordinatBD = data.koorBisaDilalui.split(" ");
         int[][] arrayBD = new int[koordinatBD.length][2];
-        int count = 0;
 
+        int count = 0;
         for (String s : koordinatBD) {
-            if (s.contains(",")) {
-                String[] xy = s.split(",");
-                int x = Integer.parseInt(xy[0]);
-                int y = Integer.parseInt(xy[1]);
-                arrayBD[count][0] = x;
-                arrayBD[count][1] = y;
-                count++;
-            }
+            String[] xy = s.split(",");
+            int x = Integer.parseInt(xy[0]);
+            int y = Integer.parseInt(xy[1]);
+            arrayBD[count][0] = x;
+            arrayBD[count][1] = y;
+            count++;
         }
 
         Koordinat start = new Koordinat(arrayBD[0][0], arrayBD[0][1]);
@@ -32,25 +63,147 @@ public class TowerDefenseLabyrinth {
 
         lab.setBisaDilalui(arrayBD, count);
         lab.setStartFinish(start, finish);
-        lab.cetakLabirin();
+
+        PriorityQueue pq = new PriorityQueue();
+
+        for (int i = 0; i < data.jmlPeserta; i++) {
+            String[] input = data.dataPeserta[i].split(" ");
+            String namaPeserta = input[0];
+
+            String[] direction = new String[input.length - 1];
+            System.arraycopy(input, 1, direction, 0, input.length - 1);
+
+            Peserta peserta = new Peserta(namaPeserta, direction, sizeLabirin);
+            boolean[][] sudahDilalui = new boolean[sizeLabirin][sizeLabirin];
+
+            peserta.steps = 0;
+            solveMaze(peserta, sudahDilalui, start.x, start.y, finish.x, finish.y, lab);
+
+            pq.enqueue(peserta);
+        }
+
+        if (team.equals("BLUE")){
+            System.out.println(team + " TEAM ENTERED RED TOWER");
+        } else {
+            System.out.println("\n" + team + " TEAM ENTERED BLUE TOWER");
+        }
+
+        Queue printQueue = new Queue();
+        Peserta fastestPeserta = null;
+
+        while (!pq.isEmpty()) {
+            Peserta p = pq.dequeue();
+            printQueue.enqueue(p);
+
+            if (fastestPeserta == null && p.steps > 0) {
+                fastestPeserta = p;
+            }
+
+            System.out.println(p.nama);
+            printLabirin(p, lab);
+        }
+
+        System.out.println();
+
+        if (fastestPeserta != null) {
+            System.out.print("FASTEST " + fastestPeserta.nama + " " + fastestPeserta.steps + " steps");
+
+            printQueue.dequeue();
+            while (!printQueue.isEmpty()) {
+                Peserta p = printQueue.dequeue();
+                if (p.steps > 0) {
+                    System.out.print("\n" + p.nama + " " + p.steps + " steps");
+                }
+            }
+        } else {
+            System.out.print("FASTEST - 0 steps");
+        }
+
+        System.out.println();
+
+        return (fastestPeserta != null) ? fastestPeserta.steps : Integer.MAX_VALUE;
+    }
+
+    static boolean solveMaze(Peserta p, boolean[][] sudahDilalui, int sx, int sy, int fx, int fy, Labirin lab) {
+        if (sx < 0 || sx >= lab.size || sy < 0 || sy >= lab.size || !lab.bisaDilalui[sx][sy] || sudahDilalui[sx][sy]) {
+            return false;
+        }
+
+        p.steps++;
+        sudahDilalui[sx][sx] = true;
+        p.pathNumbers[sx][sx] = p.steps;
+
+        if (sx == fx && sy == fy) {
+            return true;
+        }
+
+        for (int i = 0; i < p.direction.length; i++) {
+            String dir = p.direction[i];
+            int nextX = sx;
+            int nextY = sy;
+
+            switch (dir) {
+                case "UP" -> nextY++;
+                case "DOWN" -> nextY--;
+                case "RIGHT" -> nextX++;
+                case "LEFT" -> nextX--;
+            }
+
+            if (solveMaze(p, sudahDilalui, nextX, nextY, fx, fy, lab)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void printLabirin(Peserta p, Labirin lab) {
+        for (int i = lab.size - 1; i >= 0; i--) {
+            for (int j = 0; j < lab.size; j++) {
+                if (p.pathNumbers[i][j] > 0) {
+                    System.out.printf("[%02d]", p.pathNumbers[i][j]);
+                } else if (lab.bisaDilalui[i][j]) {
+                    System.out.print("[OO]");
+                } else {
+                    System.out.print("[XX]");
+                }
+            }
+            System.out.println();
+        }
     }
 }
 
-// Ini bagian untuk menyesuaikan dengan detil spesifikasi program ke-1
+class Tower {
+    int sizeLabirin;
+    String koorBisaDilalui;
+    int jmlPeserta;
+    String[] dataPeserta;
+
+    public Tower(int sizeLabirin, String koorBisaDilalui, int jmlPeserta, String[] dataPeserta) {
+        this.sizeLabirin = sizeLabirin;
+        this.koorBisaDilalui = koorBisaDilalui;
+        this.jmlPeserta = jmlPeserta;
+        this.dataPeserta = dataPeserta;
+    }
+
+}
+
+class Koordinat {
+    int x, y;
+
+    Koordinat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class Labirin {
     int size;
-    String[][] areaLabirin;
+    boolean[][] bisaDilalui;
     Koordinat start, finish;
 
     Labirin(int size) {
         this.size = size;
-        areaLabirin = new String[size][size];
-
-        for (int i = 0; i < areaLabirin.length; i++){
-            for (int j = 0; j < areaLabirin[i].length; j++){
-                areaLabirin[i][j] = "[XX]";
-            }
-        }
+        this.bisaDilalui = new boolean[size][size];
     }
 
     void setBisaDilalui(int[][] koordinatJalur, int count) {
@@ -58,7 +211,7 @@ class Labirin {
             int x = koordinatJalur[i][0];
             int y = koordinatJalur[i][1];
             if (x >= 0 && x < size && y >= 0 && y < size) {
-                areaLabirin[y][x] = "[00]";
+                bisaDilalui[y][x] = true;
             }
         }
     }
@@ -66,142 +219,103 @@ class Labirin {
     void setStartFinish(Koordinat start, Koordinat finish) {
         this.start = start;
         this.finish = finish;
-        areaLabirin[start.y][start.x] = "[ST]";
-        areaLabirin[finish.y][finish.x] = "[FN]";
-    }
-
-    void cetakLabirin(){
-        for (int i = size - 1; i >= 0; i--) {
-            for (int j = 0; j < size; j++) {
-                System.out.print(areaLabirin[i][j]);
-            }
-            System.out.println();
-        }
     }
 }
 
-class Koordinat {
-    int x, y;
+class Peserta {
+    String nama;
+    String[] direction;
+    int steps;
+    int[][] pathNumbers;
 
-    Koordinat(int x, int y){
-        this.x = x;
-        this.y = y;
+    Peserta(String nama, String[] direction, int size) {
+        this.nama = nama;
+        this.direction = direction;
+        this.steps = 0;
+        this.pathNumbers = new int[size][size];
     }
 }
 
-class NodePemain {
-    Pemain data;
-    NodePemain next;
+class Node {
+    Peserta data;
+    Node next;
 
-    NodePemain(Pemain data){
+    Node(Peserta data) {
         this.data = data;
         this.next = null;
     }
 }
 
-class NodePQ {
-    Pemain data;
-    NodePQ next;
+class Queue {
+    Node front, rear;
 
-    NodePQ(Pemain data){
-        this.data = data;
-    }
-}
-
-class QueuePemain {
-    NodePemain front, rear;
-    int size;
-
-    boolean isEmpty(){
-        return (front == null);
+    Queue() {
+        front = rear = null;
     }
 
-    int getSize() {
-        return size;
+    boolean isEmpty() {
+        return front == null;
     }
 
-    void enqueue(Pemain data){
-        NodePemain pemainBaru = new NodePemain(data);
-        if (rear == null){
-            front = rear = pemainBaru;
+    void enqueue(Peserta data) {
+        Node newNode = new Node(data);
+
+        if (rear == null) {
+            front = rear = newNode;
         } else {
-            rear.next = pemainBaru;
-            rear = pemainBaru;
+            rear.next = newNode;
+            rear = newNode;
         }
     }
 
-    Pemain dequeue() {
-        if (isEmpty()){
+    Peserta dequeue() {
+        if (isEmpty()) {
             return null;
         }
+
+        Peserta temp = front.data;
         front = front.next;
-        if (isEmpty()){
+
+        if (front == null) {
             rear = null;
         }
-        size--;
-        return front.data;
+        return temp;
     }
 }
 
 class PriorityQueue {
-    NodePQ head;
-    int size;
+    Node front;
 
     boolean isEmpty() {
-        return (head == null);
+        return front == null;
     }
 
-    int getSize() {
-        return size;
+    PriorityQueue() {
+        this.front = null;
     }
 
-    void enqueue(Pemain data){
-        NodePQ pemainBaru = new NodePQ(data);
-        if (isEmpty() || data.lebihBaikDari(head.data)){
-            pemainBaru.next = head;
-            head = pemainBaru;
+    void enqueue(Peserta peserta) {
+        Node newNode = new Node(peserta);
+
+        if (front == null || front.data.steps > peserta.steps || (front.data.steps == peserta.steps && front.data.nama.compareTo(peserta.nama) > 0)) {
+            newNode.next = front;
+            front = newNode;
         } else {
-            NodePQ sekarang = head;
-            while (sekarang.next != null && !data.lebihBaikDari(sekarang.next.data)) {
-                sekarang = sekarang.next;
+            Node current = front;
+            while (current.next != null && (current.next.data.steps < peserta.steps || (current.next.data.steps == peserta.steps && current.next.data.nama.compareTo(peserta.nama) < 0))) {
+                current = current.next;
             }
-            pemainBaru.next = sekarang.next;
-            sekarang.next = pemainBaru;
+            newNode.next = current.next;
+            current.next = newNode;
         }
-        size++;
     }
 
-    Pemain dequeue(){
-        if (isEmpty()){
+    Peserta dequeue() {
+        if (isEmpty()) {
             return null;
         }
-        Pemain data = head.data;
-        head = head.next;
-        size--;
-        return data;
-    }
-
-}
-
-class Pemain  {
-    String nama;
-    String[] gerakan;
-    int jmlLangkah = 0;
-    boolean finish = false;
-    int[][] lintasan;
-
-    Pemain(String nama, String[] gerakan){
-        this.nama = nama;
-        this.gerakan = gerakan;
-    }
-
-    boolean lebihBaikDari(Pemain lain) {
-        if (this.jmlLangkah < lain.jmlLangkah) {
-            return true;
-        }
-        if (this.jmlLangkah == lain.jmlLangkah) {
-            return this.nama.compareTo(lain.nama) < 0;
-        }
-        return false;
+        Peserta peserta = front.data;
+        front = front.next;
+        return peserta;
     }
 }
